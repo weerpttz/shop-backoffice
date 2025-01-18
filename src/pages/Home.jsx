5af83ecbe0a96e3abc44c478fed2946b06844cd2
Weerapong Worasit <weerpttz@192.168.1.18>
2025-01-18 14:20:52 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+    
+    
+    return <>
+        <h1>H O M E P A G E !! !! !!</h1>
+    </>
+}
+export default Home
---
+++ src/routes/index.jsx
@@ -0,0 +1,62 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useAuth } from "../provider/AuthProvider";
+import { ProtectedRoute } from "./ProtectedRoute";
+import Login from "../pages/Login";
+
+const Routes = () => {
+  const { token } = useAuth();
+
+  // Define public routes accessible to all users
+  const routesForPublic = [
+    {
+      path: "/",
+      element: <div>Hello this is my webSite!!!!</div>,
+    },
+    {
+      path: "/healthcheck",
+      element: <div>Website is online!</div>,
+    },
+  ];
+
+  // Define routes accessible only to authenticated users
+  const routesForAuthenticatedOnly = [
+    {
+      path: "/",
+      element: <ProtectedRoute />, // Wrap the component in ProtectedRoute
+      children: [
+        {
+          path: "/backoffice/home",
+          element: <div></div>,
+        },
+        {
+          path: "/profile",
+          element: <div>User Profile</div>,
+        },
+        {
+          path: "/logout",
+          element: <div></div>,
+        },
+      ],
+    },
+  ];
+
+  // Define routes accessible only to non-authenticated users
+  const routesForNotAuthenticatedOnly = [
+    {
+      path: "/backoffice/login",
+      element: <Login />,
+    },
+  ];
+
+  // Combine and conditionally include routes based on authentication status
+  const router = createBrowserRouter([
+    ...routesForPublic,
+    ...(!token ? routesForNotAuthenticatedOnly : []),
+    ...routesForAuthenticatedOnly,
+  ]);
+
+  // Provide the router configuration using RouterProvider
+  return <RouterProvider router={router} />;
+};
+
+export default Routes;
