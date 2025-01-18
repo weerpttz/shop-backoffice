6a33a92f7ea41c1ba5658cb23f08a620c05c0ae0
Weerapong Worasit <weerpttz@192.168.1.18>
2025-01-18 15:47:55 +0700

    edit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -39,7 +39,7 @@ const Login = () => {
     }
 
     return <>
-        <div className="flex flex-wrap items-center">
+        <div className="mx-auto">
         <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
                 <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
                 <h3 className="font-medium text-black dark:text-white">
