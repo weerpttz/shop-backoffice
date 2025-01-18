ffa6297e334cce52fd10d844197dccefd0b532f2
Weerapong Worasit <weerpttz@192.168.1.18>
2025-01-18 14:52:10 +0700

    edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+import colors from 'tailwindcss/colors'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
