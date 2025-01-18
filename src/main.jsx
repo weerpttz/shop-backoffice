0317eb3bf8ce0c98d9744ce550ebe01b45c0b53d
Weerapong Worasit <weerpttz@192.168.1.18>
2025-01-18 14:40:18 +0700

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './css/style.css'
+import './css/satoshi.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
